76bf25651b2c9c00258a686b6514a089dab83c87
Muhammad Rizqi <ikiwoioi11@gmail.com>
2022-02-03 17:31:09 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,19 +18,19 @@ global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ"
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
-global.myweb ="https://api-alphabot.herokuapp.com/"
-global.youtube = "https://youtube.com/c/ZeeoneOfc"
-global.github = "https://zeeoneofc.github.io/"
-global.ownernomer = "62887435047326"
-global.ownernomerr = "+62887435047326"
+global.botname = "eXploitxR"
+global.ownername= "Mc'Sl0vv"
+global.myweb ="https://www.idiotblackhat.com/"
+global.youtube = "https://youtube.com/SILUMANWIBU"
+global.github = "https://github.com/McSl0vv"
+global.ownernomer = "6281586841425"
+global.ownernomerr = "+6281586841425"
 global.thumbnail = "./image/lol.jpg"
 global.donasi = "./image/donasi.jpg"
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm
+global.owner = ["6281586841425","6281586841425","6281586841425"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© eXploitxR' //sticker wm
+global.author = "From Mc'Sl0vv" //sticker wm
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
